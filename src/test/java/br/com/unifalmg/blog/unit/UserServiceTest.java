0dbfb48ea2031f5d25f8bce3cf9a8d2456ff89e4
leonardosantossp <leo040604@gmail.com>
2023-12-05 12:41:21 -0300

    feat: adding a method editUser

--- src/test/java/br/com/unifalmg/blog/unit/UserServiceTest.java
+++ src/test/java/br/com/unifalmg/blog/unit/UserServiceTest.java
@@ -82,27 +82,4 @@ public class UserServiceTest {
                 ()-> Assertions.assertEquals("LeoSantos", list.get(0).getUsername())
         );
     }
-
-    @Test
-    @DisplayName("#getAllUsers > When there are no registered users > Returns an empty list")
-    void getAllUsersWhenThereAreNoRegisteredUsersReturnsAnEmptyList(){
-        List<User> response = service.getAllUsers();
-        Assertions.assertTrue(response.isEmpty());
-    }
-    @Test
-    @DisplayName("#getAllUsers > When there are registered users > Returns the list of users")
-    void getAllUsersWhenThereAreRegisteredUsersReturnsTheListOfUsers(){
-        when(service.getAllUsers()).thenReturn(List.of(User.builder()
-                .id(1)
-                .name("Leandro")
-                .username("leandroka")
-                .build()));
-        List<User> response = service.getAllUsers();
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(1,response.get(0).getId()),
-                () -> Assertions.assertEquals("Leandro",response.get(0).getName()),
-                () -> Assertions.assertEquals("leandroka",response.get(0).getUsername())
-
-        );
-    }
 }
